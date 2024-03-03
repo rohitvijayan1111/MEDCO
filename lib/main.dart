477bf94c6efa5f94106971742447f5c0e90642db
rohit-vijayan <broh22012.it@rmkec.ac.in>
2024-03-03 14:43:29 +0530

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:pdl/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: AnimatedSplashScreen(
+      splash: 'assets/image.png',
+      splashIconSize: double.infinity,
+      duration: 5000,
+      nextScreen: const Home(),
+      backgroundColor: const Color(0xFF32BFAE),
+      splashTransition: SplashTransition.scaleTransition,
+    ));
+  }
+}
