a6e1fd131447465c39d0d3c0f2ddb0564c7a73e3
rohit-vijayan <broh22012.it@rmkec.ac.in>
2024-03-04 18:16:22 +0530

    before firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:pdl/Auth.dart';
 
-void main() {
+main() {
   runApp(const MyApp());
 }
 
