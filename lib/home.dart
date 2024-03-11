b643693249b6b08737b63096dff165c58ae42666
Divakar G <divakar3501@gmail.com>
2024-03-11 21:55:00 +0530

    doctorspage

--- lib/home.dart
+++ lib/home.dart
@@ -51,64 +51,139 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 10), // Added SizedBox for spacing
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Align(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HealthButton(
-                    iconAsset: 'assets/doctors.png',
-                    label: 'Doctors',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DoctorsPage()),
-                      );
-                    },
-                  ),
-                  HealthButton(
-                    iconAsset: 'assets/hospital.png',
-                    label: 'Hospital',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HospitalPage()),
-                      );
-                    },
-                  ),
-                  HealthButton(
-                    iconAsset: 'assets/diagonisis.png',
-                    label: 'Diagnosis',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DiagonisisPage()),
-                      );
-                    },
-                  ),
-                  HealthButton(
-                    iconAsset: 'assets/ambulance.png',
-                    label: 'Ambulance',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AmbulancePage()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-
+          SizedBox(height: 10),
           Expanded(
-            child: Center(
-              child: Text('Welcome to Home Page'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HealthButton(
+                        iconAsset: 'assets/doctors.png',
+                        label: 'Doctors',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DoctorsPage()),
+                          );
+                        },
+                      ),
+                      HealthButton(
+                        iconAsset: 'assets/hospital.png',
+                        label: 'Hospital',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HospitalPage()),
+                          );
+                        },
+                      ),
+                      HealthButton(
+                        iconAsset: 'assets/diagonisis.png',
+                        label: 'Diagnosis',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DiagonisisPage()),
+                          );
+                        },
+                      ),
+                      HealthButton(
+                        iconAsset: 'assets/ambulance.png',
+                        label: 'Ambulance',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AmbulancePage()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 177, 183, 186)
+                              .withOpacity(0.6),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Early protection for your family health',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Learn More'),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 49, 196, 181),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      height: 170,
+                      child: Image.asset(
+                        'assets/homedoctor.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Top Doctors',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DoctorsPage()),
+                        );
+                      },
+                      child: Text(
+                        'See All',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 49, 196, 181),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // Add your list of top doctors here
+              ],
             ),
           ),
         ],
