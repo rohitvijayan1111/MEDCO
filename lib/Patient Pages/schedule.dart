7023e45eb557a12bec39596cd13842d7cb903755
Divakar G <divakar3501@gmail.com>
2024-04-12 23:41:07 +0530

    schedule

--- lib/Patient Pages/schedule.dart
+++ lib/Patient Pages/schedule.dart
@@ -26,78 +26,92 @@ class SchedulePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Appointments'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('Doctors') // Doctors collection
-            .doc(userId)
-            .collection('appointments')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance.collection('Users').doc(userId).snapshots(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (userSnapshot.hasError) {
+            return Center(child: Text('Error: ${userSnapshot.error}'));
           } else {
-            final appointments = snapshot.data!.docs.map((doc) {
-              final appointmentData = doc.data() as Map<String, dynamic>;
-              return Appointment(
-                doctorId: appointmentData['doctorId'],
-                patientId: appointmentData['patientId'],
-                date: (appointmentData['date'] as Timestamp).toDate(),
-                time: appointmentData['time'],
-              );
-            }).toList();
+            final isDoctor = userSnapshot.data?.exists ?? false;
+            final appointmentsCollection = isDoctor ? 'Doctors/$userId/appointments' : 'Patients/$userId/appointments';
 
-            return ListView.builder(
-              itemCount: appointments.length,
-              itemBuilder: (context, index) {
-                final appointment = appointments[index];
-                return FutureBuilder<DocumentSnapshot>(
-                  future: FirebaseFirestore.instance
-                      .collection('Patients') // Patients collection
-                      .doc(appointment.patientId)
-                      .get(),
-                  builder: (context, patientSnapshot) {
-                    if (patientSnapshot.connectionState == ConnectionState.waiting) {
-                      return ListTile(
-                        title: Text('Loading...'),
-                        subtitle: Text('Date: ${_formatDate(appointment.date)}\nTime: ${appointment.time}'),
-                      );
-                    } else if (patientSnapshot.hasError) {
-                      return ListTile(
-                        title: Text('Error loading data'),
-                      );
-                    } else {
-                      final patientData = patientSnapshot.data!.data() as Map<String, dynamic>;
-                      final patientName = patientData['name'] ?? 'Unknown';
+            return StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection(appointmentsCollection).snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  final appointments = snapshot.data!.docs.map((doc) {
+                    final appointmentData = doc.data() as Map<String, dynamic>;
+                    return Appointment(
+                      doctorId: appointmentData['doctorId'],
+                      patientId: appointmentData['patientId'],
+                      date: (appointmentData['date'] as Timestamp).toDate(),
+                      time: appointmentData['time'],
+                    );
+                  }).toList();
 
-                      return Container(
-                        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                        padding: EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Colors.grey[200],
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 3,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Patient: $patientName'),
-                            Text('Date: ${_formatDate(appointment.date)}'),
-                            Text('Time: ${appointment.time}'),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                );
+                  return appointments.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: appointments.length,
+                          itemBuilder: (context, index) {
+                            final appointment = appointments[index];
+                            final fetchId = isDoctor ? appointment.patientId : appointment.doctorId;
+                            final fetchCollection = isDoctor ? 'Patients' : 'Doctors';
+
+                            return FutureBuilder<DocumentSnapshot>(
+                              future: FirebaseFirestore.instance.collection(fetchCollection).doc(fetchId).get(),
+                              builder: (context, dataSnapshot) {
+                                if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                                  return ListTile(
+                                    title: Text('Loading...'),
+                                    subtitle: Text('Date: ${_formatDate(appointment.date)}\nTime: ${appointment.time}'),
+                                  );
+                                } else if (dataSnapshot.hasError) {
+                                  return ListTile(
+                                    title: Text('Error loading data'),
+                                  );
+                                } else {
+                                  final data = dataSnapshot.data!.data() as Map<String, dynamic>;
+                                  final name = data['name'] ?? 'Unknown';
+
+                                  return Container(
+                                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                                    padding: EdgeInsets.all(16.0),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: Colors.grey[200],
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 3,
+                                          blurRadius: 7,
+                                          offset: Offset(0, 3),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Name: $name'),
+                                        Text('Date: ${_formatDate(appointment.date)}'),
+                                        Text('Time: ${appointment.time}'),
+                                      ],
+                                    ),
+                                  );
+                                }
+                              },
+                            );
+                          },
+                        )
+                      : Center(
+                          child: Text('No appointments found.'),
+                        );
+                }
               },
             );
           }
